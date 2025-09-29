2e2b127a1a68fc8dcb9cff4d6f466a753d5d2e4a
dt-0651781453-ZtrK8 <xuchenning@inper.com>
2025-09-29 15:42:49 +0800

    test

--- ChunGeEr/main.cpp
+++ ChunGeEr/main.cpp
@@ -1,10 +1,28 @@
 #include "mainwindow.h"
-
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
 #include <QApplication>
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    tesseract::TessBaseAPI tess;
+    if (tess.Init("D:\\Program Files\\Tesseract-OCR\\tessdata", "chi_sim") != 0) {
+        return 0;
+    }
+    // 识别图片
+    QString path ="123123.jpg";
+    Pix *image = pixRead(path.toLocal8Bit().data());
+    tess.SetImage(image);
+    char *text = tess.GetUTF8Text();
+    QString ocrResult = QString::fromUtf8(text);
+    qDebug()<<ocrResult;
+    // 显示结果
+
+    // 释放资源
+    pixDestroy(&image);
+    delete[] text;
+    tess.End();
     MainWindow w;
     w.show();
     return a.exec();
